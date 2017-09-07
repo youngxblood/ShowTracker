657b245d3bef7cf0dfae6753baaf6440cf645547
crazyeight68@gmail.com <Cararoo68>
2017-09-06 21:18:27 -0700

    Added page for creation of show data.

--- resources/views/shows/create.blade.php
+++ resources/views/shows/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="col-lg-12">
-<h1 class="col-lg-4 col-lg-offset-4 well text-center">Add Show Info</h1>
+        <h1 class="col-lg-4 col-lg-offset-4 well text-center">Add Show Info</h1>
     </div>
 
 </div>
